0fb9fec9cb7260e2fd9cf6e8b7f8a5689b52295b
Denis Balushkin <balushkin.denis@yandex.ru>
2019-09-23 10:36:56 +0300

    Добавил компонент добавления нового задания

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
-
+    <NewTodo></NewTodo>
   </div>
 </template>
 
 <script>
+import NewTodo from './components/new-todo.vue'
 
 export default {
-
+  components: {
+    NewTodo
+  }
 }
 </script>
 
---
+++ src/components/new-todo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="new-todo" class="new-todo">
+    <imput class="new-todo__text"
+           :value="newTodo"
+           @change="getTodo"
+           placeholder="Новое задание">
+
+    <button class="new-todo__btn" @click="addTodo">
+      Добавить
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      getTodo(e) {
+        this.$store.dispatch('getTodo', e.target.value)
+      }
+      addTodo() {
+        this.$store.dispatch('addTodo');
+        this.$store.dispatch('clearTodo');
+      }
+    },
+    computed: {
+      newTodo() {
+        return this.$store.getters.newTodo
+      }
+    }
+  }
+</script>
