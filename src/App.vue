d1dd470b695cae8a6237db590b5bb7830e494914
Denis Balushkin <balushkin.denis@yandex.ru>
2019-09-23 11:46:37 +0300

    Добавлен компонент вывода всех заданий

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
+    <AllTodo></AllTodo>
     <NewTodo></NewTodo>
   </div>
 </template>
 
 <script>
 import NewTodo from './components/new-todo.vue'
+import AllTodo from './components/all-todo.vue'
 
 export default {
   components: {
-    NewTodo
+    NewTodo,
+    AllTodo
   }
 }
 </script>
---
+++ src/components/all-todo.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="all-todo" class="all-todo">
+        <div class="all-todo__container">
+            <div class="all-todo__item" v-for="todo in todos">
+                <button @click="complete(todo)"
+                        class="all-todo__item-complete">
+                        Complete
+                </button>
+                {{todo.body}}
+                <div class="all-todo__item-btns">
+                    <button @click="edit(todo)"
+                            class="todo-edit">
+                            Edit
+                    </button>
+
+                    <button @click="remove(todo)"
+                            class="todo-remove">
+                            Remove
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      complete(todo) {
+        this.$store.dispatch('completeTodo', todo)
+      },
+      edit(todo) {
+        this.$store.dispatch('editTodo', todo)
+      },
+      remove(todo) {
+        this.$store.dispatch('removeTodo', todo)
+      }
+    },
+    computed: {
+      todos() {
+        retirn this.$store.getetrs.todos
+      }
+    }
+  }
+</script>
