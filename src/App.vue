3d98a242ad685e93ffc8fc6d94d3a29e0cc9b3f3
Denis <balushkin.denis@yandex.ru>
2019-09-24 06:53:56 +0300

    Добавление данных. Вёртска блоков и элементов.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
-  <div id="app">
-    <AllTodo></AllTodo>
-    <NewTodo></NewTodo>
-    <CompleteTodo></CompleteTodo>
+  <div id="app" class="todo">
+    <div class="todo__block">
+      <div class="todo__block-navigation">
+        <div>Задания</div>
+        <div>Выполненные</div>
+      </div>
+      <NewTodo></NewTodo>
+      <AllTodo></AllTodo>
+      <CompleteTodo></CompleteTodo>
+    </div>
   </div>
 </template>
 
@@ -19,3 +25,34 @@ export default {
   }
 }
 </script>
+
+<style>
+  .todo {
+    width: 100%;
+    height: 100%;
+    padding: 70px 0;
+    display: flex;
+    display: -webkit-flex;
+    flex-direction: column;
+    -webkit-flex-direction: column;
+    justify-content: center;
+    -webkit-justify-content: center;
+    aloign-items: center;
+    -webkit-align-items: center;
+  }
+
+  .todo__block {
+    width: 60%;
+    max-width: 750px;
+    background: #e2e2e2;
+    border-radius: 5px;
+    padding: 20px;
+  }
+
+  .todo__block-navigation {
+    display: flex;
+    display: -webkit-flex;
+  }
+</style>
+
+
--- src/components/AllTodo.vue
+++ src/components/AllTodo.vue
@@ -12,12 +12,11 @@
                 <div class="todos-list__item-btns">
                     <button @click="edit(todo)"
                             class="todo-edit">
-                            Edit
+                        <i class="fas fa-pencil-alt"></i>
                     </button>
 
                     <button @click="remove(todo)"
                             class="todo-remove">
-                            Remove
                     </button>
                 </div>
             </div>
@@ -45,3 +44,40 @@
     }
   }
 </script>
+
+<style>
+    .todos-list {
+        padding: 20px 0;
+    }
+    .todos-list__item {
+        background: #fff;
+        border-radius: 3px;
+        border-bottom: 2px solid #999;
+        padding: 10px;
+        color: #202020;
+        font-family: 'Roboto';
+        font-weight: 300;
+        margin-bottom: 5px;
+        transition: all .2s;
+        display: flex;
+        display: -webkit-flex;
+        align-items: center;
+        -webkit-align-items: center;
+    }
+
+    .todos-list__item-complete {
+        width: 15px;
+        height: 15px;
+        border-radius: 2px;
+        border: 1px solid #999;
+        font-size: 0px;
+        margin-right: 15px;
+    }
+
+    .todos-list__item-complete:hover,
+    .todos-list__item-complete:focus {
+        background: #999;
+        cursor: pointer;
+        transition: all .2s;
+    }
+</style>
